5b0c8ec742ebfe23eaaa76fcb1f3d92b1adf91d5
Clara <ribeiroclara096@gmail.com>
2024-12-02 15:25:55 -0300

    added javascript function

--- src/script.js
+++ src/script.js
@@ -1,32 +1,95 @@
+function getUserTimeZone() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            const apiKey = "436e9e6a4f4041e28b10cae4809b8084"; // Sua chave de API
+            const apiUrl = `https://api.ipgeolocation.io/timezone?apiKey=${apiKey}&lat=${latitude}&long=${longitude}`;
+
+            fetch(apiUrl)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Falha ao buscar dados de fuso horário');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data && data.timezone) {
+                        const userTimeZone = data.timezone;
+
+                        updateUserTime(userTimeZone);
+                    } else {
+                        throw new Error('Dados de fuso horário não encontrados');
+                    }
+                })
+                .catch(error => {
+                    console.error("Erro ao obter fuso horário:", error);
+                    alert("Não foi possível obter os dados de fuso horário. Verifique sua chave de API ou tente novamente mais tarde.");
+                });
+        });
+    } else {
+        alert("Geolocalização não é suportada pelo seu navegador.");
+    }
+}
+
+function updateUserTime(timeZone) {
+    let userElement = document.querySelector("#user-time");
+    let userDateElement = userElement.querySelector(".date");
+    let userTimeElement = userElement.querySelector(".time");
+
+    let userMoment = moment().tz(timeZone);
+
+    userDateElement.innerHTML = userMoment.format("MMMM Do YYYY");
+    userTimeElement.innerHTML = userMoment.format("h:mm:ss [<small>]A[</small>]");
+}
+
 function updateTime() {
+    let newYorkElement = document.querySelector("#newyork");
+    let nyDateElement = newYorkElement.querySelector(".date");
+    let nyTimeElement = newYorkElement.querySelector(".time");
+    let nyMoment = moment().tz("America/New_York");
 
-let newYorkElement = document.querySelector("#newyork");
-let nyDateElement = newYorkElement.querySelector(".date");
-let nyTimeElement = newYorkElement.querySelector(".time");
+    nyDateElement.innerHTML = nyMoment.format("MMMM Do YYYY");
+    nyTimeElement.innerHTML = nyMoment.format("h:mm:ss [<small>]A[</small>]");
 
-let nyMoment = moment().tz("America/New_York");
+    let hongKongElement = document.querySelector("#hongkong");
+    let hongKongDateElement = hongKongElement.querySelector(".date");
+    let hongKongTimeElement = hongKongElement.querySelector(".time");
+    let hongKongMoment = moment().tz("Asia/Hong_Kong");
 
-nyDateElement.innerHTML = nyMoment.format("MMMM Do YYYY");
-nyTimeElement.innerHTML = nyMoment.format("h:mm:ss [<small>]A[</small>]");
+    hongKongDateElement.innerHTML = hongKongMoment.format("MMMM Do YYYY");
+    hongKongTimeElement.innerHTML = hongKongMoment.format("h:mm:ss [<small>]A[</small>]");
 
-let hongKongElement = document.querySelector("#hongkong");
-let hongKongDateElement = hongKongElement.querySelector(".date");
-let hongKongTimeElement = hongKongElement.querySelector(".time");
+    let brasilElement = document.querySelector("#brasilia");
+    let brasilDateElement = brasilElement.querySelector(".date");
+    let brasilTimeElement = brasilElement.querySelector(".time");
+    let brasilMoment = moment().tz("Europe/London");
 
-let hongKongMoment = moment().tz("Asia/Hong_Kong");
+    brasilDateElement.innerHTML = brasilMoment.format("MMMM Do YYYY");
+    brasilTimeElement.innerHTML = brasilMoment.format("h:mm:ss [<small>]A[</small>]");
+}
 
-hongKongDateElement.innerHTML = hongKongMoment.format("MMMM Do YYYY");
-hongKongTimeElement.innerHTML = hongKongMoment.format("h:mm:ss [<small>]A[</small>]");
-
-let brasilElement = document.querySelector("#brasilia");
-let brasilDateElement = brasilElement.querySelector(".date");
-let brasilTimeElement = brasilElement.querySelector(".time");
-
-let brasilMoment = moment().tz("America/Sao_Paulo");
-
-brasilDateElement.innerHTML = brasilMoment.format("MMMM Do YYYY");
-brasilTimeElement.innerHTML = brasilMoment.format("h:mm:ss [<small>]A[</small>]");
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace ("_", " ").split("/")[1]; 
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+<div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
+let getTimeButton = document.querySelector("#get-time");
+getTimeButton.addEventListener("click", getUserTimeZone);
